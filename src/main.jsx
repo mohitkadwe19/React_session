baf062f4cb436342e0a59cf8061d304ce800b247
mohitkadwe19 <mohitpawar530@gmail.com>
2025-01-19 19:01:50 +0530

    Add context provider for user data and update Navbar and Contact components to display username and email

--- src/main.jsx
+++ src/main.jsx
@@ -8,17 +8,29 @@ import Contact from "./Contact.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import Product from "./components/Product.jsx";
+import { createContext } from "react";
+
+export const MyContext = createContext();
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/product/:id" element={<Product />} />
-    </Routes>
-    <Footer />
+    {/* this is context provider that helps you provide the data to all the component */}
+    <MyContext.Provider
+      // value have the initial value of the context that you want to provide to all the components
+      value={{
+        username: "admin",
+        email: "admin@gmail.com",
+      }}
+    >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+      <Footer />
+    </MyContext.Provider>
   </BrowserRouter>
 );
